a1e80e3a7ff3c6a27fa5245dad71baa4a262d95a
TamSan27 <se.tamil333@gmail.com>
2021-01-30 12:24:05 +0530

    added

---
+++ src/com/learning/rough/Rough.java
@@ -0,0 +1,21 @@
+package com.learning.rough;
+
+public class Rough {
+	
+	public static void main(String[] args) {
+		
+		int[] a = {5,4,3,2,1};
+		
+		int[] b = new int[a.length-1];
+		System.out.println("leanght is:"+a.length);
+		for(int i=0;i<=a.length-2;i++) {
+			b[i] = a[i]-a[i+1];
+		}
+		System.out.print("the new array values are :{");
+
+		for(int i=0;i<b.length;i++) {
+		System.out.print(","+b[i]);
+	}
+
+}
+}
